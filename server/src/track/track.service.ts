d13544010d68cb12884571cadd382597b771aa1c
Vadim Prokoshin <vpro85@gmail.com>
2021-09-28 02:41:49 +0200

    Add REST API and data transfer objects (DTO) for tracks and comments

--- server/src/track/track.service.ts
+++ server/src/track/track.service.ts
@@ -1,29 +1,44 @@
 import {Injectable} from "@nestjs/common";
 import {Track, TrackDocument} from "./schemas/track.schema";
 import {InjectModel} from "@nestjs/mongoose";
-import {Model} from "mongoose";
+import {Model, ObjectId} from "mongoose";
 import {Comment, CommentDocument} from "./schemas/comment.schema";
+import {CreateTrackDto} from "./dto/create-track.dto";
+import {CreateCommentDto} from "./dto/create-comment.dto";
 
 @Injectable()
 export class TrackService {
 
     constructor(@InjectModel(Track.name) private trackModel: Model<TrackDocument>,
-                @InjectModel(Comment.name) private commentModel: Model<CommentDocument>) {}
-
-
-    async create() {
-
+                @InjectModel(Comment.name) private commentModel: Model<CommentDocument>) {
     }
 
-    async getAll() {
 
+    async create(dto: CreateTrackDto): Promise<Track> {
+        const track = await this.trackModel.create({...dto, listens: 0})
+        return track
     }
 
-    async getOne() {
-
+    async getAll(): Promise<Track[]> {
+        const tracks = await this.trackModel.find()
+        return tracks
     }
 
-    async delete() {
+    async getOne(id: ObjectId): Promise<Track> {
+        const track = await this.trackModel.findById(id).populate('comments')
+        return track
+    }
 
+    async delete(id: ObjectId): Promise<ObjectId> {
+        const track = await this.trackModel.findByIdAndDelete(id)
+        return track._id
+    }
+
+    async addComment(dto: CreateCommentDto): Promise<Comment>{
+        const track = await this.trackModel.findById(dto.trackId)
+        const comment = await this.commentModel.create({...dto})
+        track.comments.push(comment._id)
+        await track.save()
+        return comment
     }
 }
