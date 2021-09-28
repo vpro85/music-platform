11025ab258fdf41bac15c0a03555586bb3f988bb
Vadim Prokoshin <vpro85@gmail.com>
2021-09-28 06:50:30 +0200

    Add track upload (audio and image), add pagination, add search and increment for 'listen'

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,16 @@
 import {Module} from "@nestjs/common";
 import {TrackModule} from "./track/track.module";
 import {MongooseModule} from "@nestjs/mongoose";
+import {FileModule} from "./file/file.module";
+import {ServeStaticModule} from "@nestjs/serve-static";
+import * as path from "path";
 
 @Module({
     imports: [
+        ServeStaticModule.forRoot({rootPath: path.resolve(__dirname, 'static')}),
         MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0.otqm2.mongodb.net/music-platform?retryWrites=true&w=majority'),
-        TrackModule
+        TrackModule,
+        FileModule
     ]
 })
 
--- server/src/track/track.module.ts
+++ server/src/track/track.module.ts
@@ -4,6 +4,7 @@ import {TrackService} from "./track.service";
 import {MongooseModule} from "@nestjs/mongoose";
 import {Track, TrackSchema} from "./schemas/track.schema";
 import {Comment, CommentSchema} from "./schemas/comment.schema";
+import {FileService} from "../file/file.service";
 
 @Module({
     imports: [
@@ -12,7 +13,7 @@ import {Comment, CommentSchema} from "./schemas/comment.schema";
 
     ],
     controllers: [TrackController],
-    providers: [TrackService]
+    providers: [TrackService, FileService]
 })
 
 export class TrackModule {
