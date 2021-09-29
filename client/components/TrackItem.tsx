7faeaf3464190581db739eaaf473c836e6a02aa1
Vadim Prokoshin <vpro85@gmail.com>
2021-09-29 02:49:27 +0200

    Add detailed track view page (without db interaction)

--- client/components/TrackItem.tsx
+++ client/components/TrackItem.tsx
@@ -3,6 +3,7 @@ import {ITrack} from "../types/tracks";
 import {Card, Grid, IconButton} from "@material-ui/core";
 import styles from '../styles/TrackItem.module.scss'
 import {Delete, Pause, PlayArrow} from "@mui/icons-material";
+import {useRouter} from "next/router";
 
 interface TrackItemProps {
     track: ITrack;
@@ -11,9 +12,11 @@ interface TrackItemProps {
 }
 
 const TrackItem: React.FC<TrackItemProps> = ({track, active = false}) => {
+    const router = useRouter()
+
     return (
-        <Card className={styles.track}>
-            <IconButton>
+        <Card className={styles.track} onClick={() => router.push('/tracks/' + track._id)}>
+            <IconButton onClick={e => e.stopPropagation()}>
                 {active ? <Pause/> : <PlayArrow/>}
             </IconButton>
             <img width={70} height={70} src={track.picture} alt={track.name}/>
@@ -22,7 +25,7 @@ const TrackItem: React.FC<TrackItemProps> = ({track, active = false}) => {
                 <div style={{fontSize: 12, color: "gray"}}>{track.artist}</div>
             </Grid>
             {active && <div>02:42 / 03:22</div>}
-            <IconButton style={{marginLeft: "auto"}}>
+            <IconButton onClick={e => e.stopPropagation()} style={{marginLeft: "auto"}}>
                 <Delete/>
             </IconButton>
         </Card>
---
+++ client/pages/tracks/[id].tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {ITrack} from "../../types/tracks";
+import MainLayout from "../../layouts/MainLayout";
+import {Button, Grid, TextField} from "@material-ui/core";
+import {useRouter} from "next/router";
+
+const TrackPage = () => {
+    const router = useRouter()
+    const track: ITrack = {
+        _id: '1',
+        artist: 'IF',
+        text: 'Something to sing...',
+        name: 'Track 1',
+        comments: [],
+        listens: 0,
+        audio: 'http://192.168.1.13:5000/audio/5a84022e-d330-4447-815a-ac2710b71fc9.mp3',
+        picture: 'http://192.168.1.13:5000/image/e3be65d1-31a6-4586-980b-966bcc019336.jpg'
+    }
+    return (
+        <MainLayout>
+            <Button
+                variant={"outlined"}
+                style={{fontSize: 32}}
+                onClick={() => router.push('/tracks')}>
+                To track list
+            </Button>
+            <Grid container style={{margin: '20px 0'}}>
+                <img src={track.picture} alt={track.name} width={200} height={200}/>
+                <div style={{marginLeft: 30}}>
+                    <h1>Title: {track.name}</h1>
+                    <h1>Artist: {track.artist}</h1>
+                    <h1>Listens: {track.listens}</h1>
+                </div>
+            </Grid>
+            <h1>Lyrics:</h1>
+            <p>{track.text}</p>
+            <h1>Comments: </h1>
+            <Grid container>
+                <TextField label={"Your name..."} fullWidth/>
+                <TextField label={"Your comment..."} fullWidth multiline rows={4}/>
+                <Button variant={"outlined"}>Post</Button>
+            </Grid>
+            <div>{track.comments.map(comment =>
+                <div>
+                    <div>Name: {comment.username}</div>
+                    <div>Comment: {comment.text}</div>
+                </div>
+            )}</div>
+        </MainLayout>
+    );
+};
+
+export default TrackPage;
